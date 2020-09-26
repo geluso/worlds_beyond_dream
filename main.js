1bdc632d78033c1fcc2befe88efd7d1ff1c510f6
Steve Geluso <stevegeluso@gmail.com>
2020-09-26 00:19:06 -0700

    adding menu mp3

--- main.js
+++ main.js
@@ -5,11 +5,9 @@ let app = express();
 
 app.post('/voice', (req, res) => {
   // Set the url of the song we are going to play
-  let songUrl = 'http://5tephen.com/owen/bikepolo.mp3'
+  let songUrl = 'http://5tephen.com/owen/mp3real/menu.mp3'
 
   const twiml = new VoiceResponse();
-
-  twiml.say("you're listening to 'Bike Polo' by 'Owen.' enjoy")
 
   twiml.play({
     loop: 1
