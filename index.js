94932a716da9d2712401035bf6ed648e75f10fde
Steve Geluso <stevegeluso@gmail.com>
2020-09-30 17:25:43 -0700

    add contest controls

--- index.js
+++ index.js
@@ -5,21 +5,37 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 const app = express();
 
+let IS_CONTEST_ENABLED = false;
 let CALLER_NUMBER = 0;
 let CALLER_WIN_NUMBER = 5;
 
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
 
+app.get('/see-caller-count', (req, res) => {
+  let output = '';
+  output += '<p>contest running?: ' + (IS_CONTEST_ENABLED ? 'yes' : 'no') + '</p>'
+  output += "<div><form method='POST' action='/toggle-contest'><button>toggle contest on/off</button></form></div>"
+  output += '<p>number of calls: ' + CALLER_NUMBER + '</p>'
+  output += "<div><form method='POST' action='/reset-callers-to-zero'><button>reset callers to zero</button></form></div>"
+  output += "<div><form method='POST' action='/set-caller-winner-number'><label>winning call number: <input type='number' step='1' value='" + CALLER_WIN_NUMBER + "' name='number'/><button>set</button></label></form></div>"
+  res.send(output);
+})
+
+app.post('/toggle-contest', (req, res) => {
+  IS_CONTEST_ENABLED = !IS_CONTEST_ENABLED
+  res.redirect('/see-caller-count')
+})
+
 app.post('/reset-callers-to-zero', (req, res) => {
   resetCallersToZero()
-  res.sendStatus(200)
+  res.redirect('/see-caller-count')
 })
 
 app.post('/set-caller-winner-number', (req, res) => {
   const number = parseInt(req.body.number)
   setCallerWinnerNumber(number)
-  res.sendStatus(200)
+  res.redirect('/see-caller-count')
 })
 
 function resetCallersToZero() {
@@ -40,7 +56,7 @@ app.post('/voice', (request, response) => {
   // If the user entered digits, process their request
   const digit = request.body.Digits
 
-  if (digit == 9) {
+  if (IS_CONTEST_ENABLED && digit == 9) {
     console.log('caller number!');
     incrementCallerNumber(twiml);
   } else {
@@ -83,5 +99,5 @@ function incrementCallerNumber(twiml) {
 }
 
 let port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Listening at http://localhost:3000'));
+app.listen(port, () => console.log('Listening at http://localhost:3000/see-caller-count'));
 
